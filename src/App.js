f7778b4869112624c53ce199aad72e5da4bb39c4
vinki100z <vikrantchauhancf2021@gmail.com>
2024-06-22 12:38:30 +0530

    Updated navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
+// App.js
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Notes from './components/Notes';
+import About from './components/About';
 
-import './App.css';
-
-import React, { useState,useEffect } from 'react'
 function App() {
-  const f=()=>{
-    console.log("hello")  
-    setCount(count+1);
-  }
-  const [count, setCount] = useState(0)
-  useEffect(() => {
-    // setCount(count+1);
-    f();
-   // eslint-disable-next-line
-  },[]);
-  
   return (
- <>
- <div>
-  <p style={{cursor:"pointer"}} onClick={()=>{setCount(count+1)}}>Counssst: {count}</p>
- </div>
- </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Notes />} />
+        <Route path="/" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Notes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Notes() {
+  return (
+    <>
+    <h1>
+        this is me
+    </h1>
+    </>
+  )
+}
